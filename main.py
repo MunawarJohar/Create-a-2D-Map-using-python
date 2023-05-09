711d87f6c1de1c0511a410c81cb4aef43881f880
Munawar Johar <106137102+MunawarJohar@users.noreply.github.com>
2023-05-09 11:14:18 +0500

    Update main.py
    Adding function on this repo

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+#adding functions on this 
+def curve():
+  for j in range(200):
+    pen.right(1)
+    pen.forward(1)
+
+
+def heart():
+  pen.fillcolor("red")
+  pen.begin_fill()
+  pen.left(50)
+  pen.forward(100)
+  pen.circle(40,180)
+  pen.left(260)
+  pen.circle(40,180)
+  pen.forward(100)
+  pen.end_fill()
+
+def text1():
+  pen.up()
+  pen.setpos(-68,95)
+  pen.down()
+  pen.color("green")
+  pen.write("I Love you M")
+
+
+
 #turtle is a python module 
 
 import turtle
